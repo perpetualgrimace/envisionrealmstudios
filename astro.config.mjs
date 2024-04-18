8a7079c78827b44a957f38ffd671c475df6629aa
perpetualgrimace <hello@jamesferrell.me>
2024-04-18 18:10:11 -0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), mdx()],
+  devToolbar: { enabled: false },
+});
